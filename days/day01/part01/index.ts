ab29ed3c6ed186308f2d00345a97e9f62764676a
Maji <steeven.regnault@outlook.fr>
2022-12-01 19:25:45 +0100

    feat: prettier

--- days/day01/part01/index.ts
+++ days/day01/part01/index.ts
@@ -3,11 +3,11 @@ const input = process.argv[2];
 const inventories = input.split('\n\n');
 let bestCalories = 0;
 
-inventories.forEach(inventory => {
+inventories.forEach((inventory) => {
   const invSum = inventory.split('\n').reduce((prev, curr) => prev + Number(curr), 0);
-  if(invSum > bestCalories) {
+  if (invSum > bestCalories) {
     bestCalories = invSum;
   }
 });
 
-console.log(bestCalories)
+console.log(bestCalories);
--- days/day01/part02/index.ts
+++ days/day01/part02/index.ts
@@ -3,13 +3,13 @@ const input = process.argv[2];
 const inventories = input.split('\n\n');
 const caloriesPerEfl: number[] = [];
 
-inventories.forEach(inventory => {
+inventories.forEach((inventory) => {
   const invSum = inventory.split('\n').reduce((prev, curr) => prev + Number(curr), 0);
   caloriesPerEfl.push(invSum);
 });
 
-const sortedCaloriesPerEfl = caloriesPerEfl.sort((a, b)=> b - a);
+const sortedCaloriesPerEfl = caloriesPerEfl.sort((a, b) => b - a);
 const podiumCalories = sortedCaloriesPerEfl.slice(0, 3);
 const podiumCaloriesSum = podiumCalories.reduce((prev, curr) => prev + curr, 0);
 
-console.log(podiumCaloriesSum)
+console.log(podiumCaloriesSum);
