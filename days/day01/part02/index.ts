70f6f8b149ee947aba2b8a7ee0b79af4067ee174
Maji <steeven.regnault@outlook.fr>
2022-12-01 19:13:29 +0100

    day-01

---
+++ days/day01/part02/index.ts
@@ -0,0 +1,15 @@
+const input = process.argv[2];
+
+const inventories = input.split('\n\n');
+const caloriesPerEfl: number[] = [];
+
+inventories.forEach(inventory => {
+  const invSum = inventory.split('\n').reduce((prev, curr) => prev + Number(curr), 0);
+  caloriesPerEfl.push(invSum);
+});
+
+const sortedCaloriesPerEfl = caloriesPerEfl.sort((a, b)=> b - a);
+const podiumCalories = sortedCaloriesPerEfl.slice(0, 3);
+const podiumCaloriesSum = podiumCalories.reduce((prev, curr) => prev + curr, 0);
+
+console.log(podiumCaloriesSum)
